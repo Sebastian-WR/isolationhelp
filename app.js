4f03e6a5beb2afd340ed8b4c6e08caaa4b293818
Cecilie <cecilie@brandt.net>
2021-05-23 14:40:32 +0200

    added error page and favicon

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ const testHtml = fs.readFileSync(__dirname + '/public/test/test.html', 'utf-8')
 const loginHtml = fs.readFileSync(__dirname + '/public/login/login.html', 'utf-8')
 const myTaskHtml = fs.readFileSync(__dirname + '/public/myTasks/myTasks.html', 'utf-8')
 const taskHtml = fs.readFileSync(__dirname + '/public/tasks/tasks.html', 'utf-8')
+const errorHtml = fs.readFileSync(__dirname + '/public/error/error.html', 'utf-8')
 
 const testPage = baseTemplate.replace('{{BODY}}', testHtml)
 const loginPage = baseTemplate.replace('{{BODY}}', loginHtml)
 const myTasksPage = baseTemplate.replace('{{BODY}}', myTaskHtml)
 const tasksPage = baseTemplate.replace('{{BODY}}', taskHtml)
+const errorPage = baseTemplate.replace('{{BODY}}', errorHtml)
 
 app.get('/', (req, res) => {
     res.send(testPage)
@@ -37,6 +39,9 @@ app.get('/chats', (req, res) => {
 app.get('/tasks', (req, res) => {
     res.send(tasksPage)
 })
+app.get('/tasks/:id', (req, res) => {
+    res.send(tasksPage)
+})
 
 app.get('/mytasks', (req, res) => {
     res.send(myTasksPage)
@@ -44,6 +49,10 @@ app.get('/mytasks', (req, res) => {
 
 app.get('/settings', (req, res) => {
     res.send(testPage)
+})
+
+app.get('/*', (req, res) => {
+    res.status(404).send(errorPage)
 })
 
 const server = app.listen(process.env.PORT || 3000, (error) => {
